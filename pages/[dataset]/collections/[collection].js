15fb4fb221e5dbdda9f8efc8dffb38ad9bb4d183
Anton Hardock <antonhardock@web.de>
2022-11-11 18:36:01 +0100

    implement extent into mock api

--- pages/[dataset]/collections/[collection].js
+++ pages/[dataset]/collections/[collection].js
@@ -2,21 +2,17 @@ import Collection from "../../../components/Collection.js"
 
 export async function getServerSideProps(context) {
     
-  const { dataset, collection } = context.query;
+  const { dataset, collection } = context.query;  
+  const url = `http://localhost:3333/api/datasets/${dataset}/collections/${collection}`
+  const res = await fetch(url);
+  const data = await res.json();
+  const { bbox, epsg } = data.spatialExtent;
   
-  // demo data (taken from props)
-  const bboxArray = [9.79328486755653, 53.433454402709614, 10.217786703966109, 53.70748874064595]
-  const epsgSource = 'EPSG:4326'
-  
-  //nothing to fetch for now, so simply return current collection from query parameter
-  //const url = `http://localhost:3333/api/datasets/${dataset}/collections/${collection}`
-  //const res = await fetch(url);
-  //const data = await res.json();
   return {
     props: {
       collection: collection,
-      bboxArray: bboxArray,
-      epsgSource: epsgSource
+      bboxArray: bbox,
+      epsgSource: epsg
     }
   }
 }
