36375c32e4aa877b2e87d9ec27d08721f402b883
Anton Hardock <antonhardock@web.de>
2023-02-18 12:18:34 +0100

    route directly to items api endpoint when f=json search parameter is used

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/:dataset/collections/:collection/items',
+          destination: 'https://api.hamburg.de/datasets/v1/:dataset/collections/:collection/items?f=json',
+          has: [{ type: 'query', key: 'f', value: "json" }],
+        }
+      ]
+    }
+  }
 }
 
 module.exports = nextConfig
